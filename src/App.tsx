c30a893c4b569a9fdb86a894ec998cdecea86c59
Nick Pachulski <nick@pachulski.me>
2019-03-15 20:30:54 -0400

    Add prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import * as React from "react";
-import { Text, View } from "react-native";
+import * as React from "react"
+import { Text, View } from "react-native"
 
 export default () => (
   <View style={{ marginTop: 50 }}>
     <Text>Hello Expo!</Text>
   </View>
-);
+)
