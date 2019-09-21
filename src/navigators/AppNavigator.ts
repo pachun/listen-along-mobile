2bd63d5c7596fda78ced4163e3b3eac0c1d8f438
Nick Pachulski <nick@pachulski.me>
2019-09-21 18:55:41 -0400

    Upgrade from Expo SDK 34 to Expo SDK 35

--- src/navigators/AppNavigator.ts
+++ src/navigators/AppNavigator.ts
@@ -1,4 +1,5 @@
-import { createAppContainer, createStackNavigator } from "react-navigation"
+import { createAppContainer } from "react-navigation"
+import { createStackNavigator } from "react-navigation-stack"
 
 import SpotifyUsersScreen from "../screens/SpotifyUsersScreen"
 import AuthenticationScreen from "../screens/AuthenticationScreen"
